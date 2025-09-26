467bf1d24b984fd070f239530fd0c3d599f9a2a4
Jannik <jannik.reisener@web.de>
2025-09-26 13:42:43 +0200

    implement ui of the question form

--- lib/validations.ts
+++ lib/validations.ts
@@ -57,13 +57,13 @@ export const AskQuestionSchema = z.object({
       message: "Title must be at least 5 characters.",
     })
     .max(130, { message: "Title musn't be longer then 130 characters." }),
-  content: z.string().min(100, { message: "Minimum of 100 characters." }),
+  content: z.string().min(1, { message: "Body is required." }),
   tags: z
     .array(
       z
         .string()
-        .min(1, { message: "Tag must have at least 1 character." })
-        .max(15, { message: "Tag must not exceed 15 characters." })
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag cannot exceed 30 characters." })
     )
     .min(1, { message: "Add at least one tag." })
     .max(3, { message: "Maximum of 3 tags." }),
